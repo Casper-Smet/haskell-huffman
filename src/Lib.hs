a2b45fe8260aedf77184c7177d130043a761dc33
Casper Smet <casper.smet@student.hu.nl>
2019-10-26 11:42:12 +0200

    Added instance ord, new countValues, pattern matching

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,35 +1,29 @@
 module Lib
-    ( someFunc
-    , countValues
+    ( countValues
     , createNodes
     , createTree
     ) where
 
-import Data.List (group, sort, sortOn, sortBy)
-import Data.Ord (comparing)
+import Data.List (group, sort)
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
 
-data HuffmanTree = Node {character :: Char, weight :: Int} | Branch {left :: HuffmanTree, right :: HuffmanTree, weight :: Int} deriving (Show)
+data HuffmanTree = Node {character :: Char, weight :: Int} | Branch {left :: HuffmanTree, right :: HuffmanTree, weight :: Int} deriving (Show, Eq)
+
+instance Ord HuffmanTree where
+    compare x y = compare (weight x) (weight y)
 
 -- Adapted from https://stackoverflow.com/a/3711137
 countValues :: String -> [(Char, Int)]
--- TODO: remove sortOn
-countValues xss = sortOn snd $ map (\xs@(x:_) -> (x, length xs)) $ group $ sort xss
-
-sortHuffman :: [HuffmanTree] -> [HuffmanTree]
-sortHuffman = sortOn weight
+countValues = map (\xs@(x:_) -> (x, length xs)) . group . sort
 
 createNodes :: [(Char, Int)] -> [HuffmanTree]
-createNodes xs = sortHuffman [uncurry Node x | x <- xs]
+createNodes xs = sort [uncurry Node x | x <- xs]
 
 mergeNodes t1 t2 = Branch t1 t2 $ weight t1 + weight t2
 
 createTree :: [HuffmanTree] -> HuffmanTree
-createTree (t1:t2:[]) = mergeNodes t1 t2
-createTree (t1:t2:ts) = createTree $ sortHuffman $ mergeNodes t1 t2 : ts
-
+createTree [t1, t2] = mergeNodes t1 t2
+createTree (t1:t2:ts) = createTree $ sort $ mergeNodes t1 t2 : ts
 
 test x = createTree $ createNodes $ countValues x
 
