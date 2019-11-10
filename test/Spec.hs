112b98a30b7591893e0d6283eea604dc18c724c2
Casper Smet <casper.smet@student.hu.nl>
2019-11-10 21:04:22 +0100

    Unit testing with HUnit for release 1.1

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,7 +2,8 @@
 import Test.HUnit
 import Data.Map (Map, fromList)
 
-import Lib ( countValues
+import Lib 
+    ( countValues
     , createNodes
     , createTree
     , encodeTree
@@ -12,7 +13,23 @@ import Lib ( countValues
     , codedString
     , mergeNodes
     , Binary(I, O)
-    , HuffmanTree(Node, Branch))
+    , HuffmanTree(Node, Branch)
+    )
+
+import HFIO 
+    ( keyFromString
+    , reshapeCodes
+    , formatCodes
+    , listifyCodes
+    , readCodes
+    , readCodeString
+    )
+    -- db   db db    db d8b   db d888888b d888888b 
+    -- 88   88 88    88 888o  88   `88'   `~~88~~' 
+    -- 88ooo88 88    88 88V8o 88    88       88    
+    -- 88~~~88 88    88 88 V8o88    88       88    
+    -- 88   88 88b  d88 88  V888   .88.      88    
+    -- YP   YP ~Y8888P' VP   V8P Y888888P    YP    
 
 nodeList :: [HuffmanTree]
 nodeList = [Node 'h' 1, Node 'u' 1, Node 'f' 2]
@@ -26,6 +43,7 @@ enMap = fromList [('f',[I]),('h',[O,O]),('u',[O,I])]
 deMap :: Map [Binary] Char
 deMap = fromList [([I], 'f'),([O,O], 'h'),([O,I],'u')]
 
+-- Lib.hs
 -- | countValues "huffman" == [('H',1),('a',1),('f',2),('m',1),('n',1),('u',1)]
 countValuesTest :: Test
 countValuesTest = TestCase (assertEqual "countValues" [('H',1),('a',1),('f',2),('m',1),('n',1),('u',1)] (countValues "Huffman"))
@@ -58,16 +76,49 @@ decodeListTest = TestCase (assertEqual "decodeList" "fuh" (decodeList [I,O,I,O,O
 codedStringTest :: Test
 codedStringTest = TestCase (assertEqual "codedString" [I,O,O,I,I,I,O,I,O,I,I,O,I,I,I,I,I,I,O,I,O,I,O,O,O,I,O,O,I,O,I,O,I,I,O,O,I,O,I,I,I,I,I,O,O,O,I,O,O,O] (codedString "huffman coding"))
 
+-- HFIO.hs
+-- | keyFromString "IOIO" == [I,O,I,O]
+keyFromStringTest :: Test
+keyFromStringTest = TestCase (assertEqual "keyFromString" [I,O,I,O] (keyFromString "IOIO"))
+
+-- | reshapeCodes [('c', [I,O,I])] == [('c', "IOI")]
+reshapeCodesTest :: Test
+reshapeCodesTest = TestCase (assertEqual "reshapeCodes" [('c', "IOI")] ( reshapeCodes [('c', [I,O,I])]))
+
+-- | formatCodes [('c', "IOI"), ('d', "IIO")] == "c||IOI#|/d||IIO"
+formatCodesTest :: Test
+formatCodesTest = TestCase (assertEqual "formatCodes" "c||IOI#|d||IIO" (formatCodes [('c', "IOI"), ('d', "IIO")]))
+
+-- | listifyCodes "c||IOI#|/d||IIO" == [["c", "IOI"], ["d", "IIO"]]
+listifyCodesTest :: Test
+listifyCodesTest = TestCase (assertEqual "listifyCodes" [["c", "IOI"], ["d", "IIO"]] (listifyCodes "c||IOI#|d||IIO"))
+
+-- | readCodes  [["c", "IOI"], ["d", "IIO"]] == [([I,O,I], 'c'), ([I,I,O], 'd')]
+readCodesTest :: Test
+readCodesTest = TestCase (assertEqual "readCodes" [([I,O,I], 'c'), ([I,I,O], 'd')] (readCodes  [["c", "IOI"], ["d", "IIO"]]))
+
+-- | readCodeString "c||IOI#|d||IIO" ==  [([I,O,I], 'c'), ([I,I,O], 'd')]
+readCodeStringTest :: Test
+readCodeStringTest = TestCase (assertEqual "readCodeString" [([I,O,I], 'c'), ([I,I,O], 'd')] (readCodeString "c||IOI#|d||IIO"))
+
+
 tests = TestList [
     -- Lib tests
-    TestLabel "countValues" countValuesTest,
-    TestLabel "createNodes" createNodesTest, 
-    TestLabel "mergeNodes" mergeNodesTest,
-    TestLabel "createTree" createTreeTest,
-    TestLabel "encodeTree" encodeTreeTest,
-    TestLabel "encodeString" encodeStringTest,
-    TestLabel "decodeList" decodeListTest,
-    TestLabel "codedString" codedStringTest
+    TestLabel "Lib.hs(countValues)" countValuesTest,
+    TestLabel "Lib.hs(createNodes)" createNodesTest, 
+    TestLabel "Lib.hs(mergeNodes)" mergeNodesTest,
+    TestLabel "Lib.hs(createTree)" createTreeTest,
+    TestLabel "Lib.hs(encodeTree)" encodeTreeTest,
+    TestLabel "Lib.hs(encodeString)" encodeStringTest,
+    TestLabel "Lib.hs(decodeList)" decodeListTest,
+    TestLabel "Lib.hs(codedString)" codedStringTest,
+    -- HFIO test
+    TestLabel "HFIO.hs(keyFromString)" keyFromStringTest,
+    TestLabel "HFIO.hs(reshapeCodes)" readCodeStringTest,
+    TestLabel "HFIO.hs(formatCodes)" formatCodesTest,
+    TestLabel "HFIO.hs(listifyCodes)" listifyCodesTest,
+    TestLabel "HFIO.hs(readCodes)" readCodesTest,
+    TestLabel "HFIO.hs(readCodeString)" readCodeStringTest
     ]
 
 main :: IO Counts
